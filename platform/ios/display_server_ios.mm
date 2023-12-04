b57ed2334fbe5394e22f7a5c2d060ead646b6b3d
Zach Coleman <ztc0611@gmail.com>
2023-12-04 18:44:01 -0500

    Update display_server_ios.mm
    Co-authored-by: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>

--- platform/ios/display_server_ios.mm
+++ platform/ios/display_server_ios.mm
@@ -443,7 +443,6 @@ int DisplayServerIOS::screen_get_dpi(int p_screen) const {
 }
 
 void DisplayServerIOS::ios_set_ui_state(bool p_hide_home_indicator, bool p_suppress_bottom_ui_gesture, bool p_suppress_top_ui_gesture, bool p_hide_status_bar, float p_status_bar_fade_time) {
-
 	ProjectSettings::get_singleton()->set_setting("display/window/ios/hide_home_indicator", hide_home_indicator);
 	ProjectSettings::get_singleton()->set_setting("display/window/ios/suppress_bottom_ui_gesture", suppress_bottom_ui_gesture);
 	ProjectSettings::get_singleton()->set_setting("display/window/ios/suppress_top_ui_gesture", suppress_top_ui_gesture);
